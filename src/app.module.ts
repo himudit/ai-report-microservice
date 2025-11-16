2c18e80974c5ed49704b127f1aac0cb61759a27d
himudit <gargmudit662@gmail.com>
2025-11-16 15:43:11 +0530

    Starting with GeminiModel

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
+import { AiModule } from './ai/ai.module';
 
 @Module({
-  imports: [UserModule, SchedulerModule],
+  imports: [UserModule, SchedulerModule, AiModule],
   controllers: [AppController],
   providers: [AppService],
 })
